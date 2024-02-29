ecd611bba7f2918a6cf1d8f57728cf8a06c097f7
Janak <tjanak001@gmail.com>
2024-02-29 16:43:10 +0545

    music app

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import AlbumItem from "./AlbumItem";
+import Slider from "./Slider";
+
+const MainSection = () => {
+  const [albums, setAlbums] = useState([]);
+  const [trending, setTrending] = useState([]);
+
+  const getHomePageData = async () => {
+    try{
+      const res = await axios.get("https://saavn.dev/modules?language=hindi,english");
+    const { data } = res.data;
+    setAlbums(data.albums);
+    setTrending(data.trending);
+    } catch(error){
+      console.log("Error fetching",error);
+    }
+    
+  };
+
+  useEffect(() => {
+    getHomePageData();
+  }, []);
+
+  const trendingAlbums = useMemo(
+    () => (Array.isArray(trending.albums) ? trending.albums : []),
+    [trending.albums]
+  );
+
+  return (
+    <section className="my-20">
+      <h2 className="text-xl px-5 py-3 font-semibold text-gray-700 w-full lg:w-[78vw] mx-auto">
+        Trending Now
+      </h2>
+      <Slider data={trendingAlbums} />
+      <h2 className="text-xl px-5 py-3 font-semibold text-gray-700 w-full lg:w-[78vw] mx-auto">
+        Top Albums
+      </h2>
+      <Slider data={albums}/>
+    </section>
+  );
+};
+
+export default MainSection;
